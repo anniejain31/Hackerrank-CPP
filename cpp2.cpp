8066ddf6473c28c8b479b52ece394f5a438b1255
Annie Jain <87299172+anniejain31@users.noreply.github.com>
2022-07-05 15:18:32 +0530

    Add files via upload

---
+++ cpp2.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a;
+    int b;
+    int c;
+    int s;
+    cin >> a>> b>> c;
+    s= a+b+c;
+    cout << s;
+    return 0;
+}
